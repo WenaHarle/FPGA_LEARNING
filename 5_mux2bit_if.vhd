7f4fc471ecf79a6dcc91d4c99afd3ca8f37a3295
Harilegawa <120030681+WenaHarle@users.noreply.github.com>
2025-02-17 18:38:28 +0700

    Update 5_mux2bit_if.vhd

--- 5_mux2bit_if.vhd
+++ 5_mux2bit_if.vhd
@@ -1,19 +1,23 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity mux2bit is
-    port (
-        a : in STD_LOGIC;
-        b : in STD_LOGIC;
-        s : in STD_LOGIC;
-        f : out STD_LOGIC
-        
+entity mux_2to1 is
+    Port (
+        sel : in std_logic;  -- Select signal
+        a   : in std_logic;  -- Input 1
+        b   : in std_logic;  -- Input 2
+        y   : out std_logic  -- Output
     );
-end mux2bit;
+end mux_2to1;
 
-architecture Behavioral of mux2bit is
+architecture behavioral of mux_2to1 is
 begin
- f <= (not s and a) or (s and b);
- 
-end Behavioral;
-
+    process (sel, a, b)
+    begin
+        if sel = '0' then
+            y <= a;
+        else
+            y <= b;
+        end if;
+    end process;
+end behavioral;
